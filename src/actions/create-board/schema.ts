7dda7b5bc7958d8092da27c719a56312a8d193ee
Ignacio Fernandez <ifernandezcerro@gmail.com>
2024-01-13 13:31:48 +0100

    form

---
+++ src/actions/create-board/schema.ts
@@ -0,0 +1,11 @@
+import {z} from 'zod';
+
+export const CreateBoard = z.object({
+  title: z.string({
+    required_error: 'Please enter a title',
+    invalid_type_error: 'Please enter a valid title',
+  }).min(3, {
+    message: 'Title must be at least 3 characters',
+  })
+});
+
