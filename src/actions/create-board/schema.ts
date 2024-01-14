61e7b6ab48c38d23898266fdf8ab052a3ccb8c7a
Ignacio Fernandez <ifernandezcerro@gmail.com>
2024-01-14 16:45:29 +0100

    organization id page

--- src/actions/create-board/schema.ts
+++ src/actions/create-board/schema.ts
@@ -1,11 +1,14 @@
-import {z} from 'zod';
+import { z } from "zod";
 
 export const CreateBoard = z.object({
   title: z.string({
-    required_error: 'Please enter a title',
-    invalid_type_error: 'Please enter a valid title',
+    required_error: "Title is required",
+    invalid_type_error: "Title is required",
   }).min(3, {
-    message: 'Title must be at least 3 characters',
-  })
+    message: "Title is too short."
+  }),
+  image: z.string({
+    required_error: "Image is required",
+    invalid_type_error: "Image is required",
+  }),
 });
-
